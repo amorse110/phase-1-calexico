a59fbd377f8fbe121857e6b809bb2c8846e822f9
Austin Morse <amorse110@gmail.com>
2023-06-21 13:41:54 -0700

    first deliverable

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here...
+const menuAPI = "http://localhost:3000/menu";
+const menuList = document.getElementById('menu-items');
+
+fetch(menuAPI)
+.then(res => res.json())
+.then(renderMenuItems)
+
+function renderMenuItems(items) {
+    items.forEach(renderInMenu)
+}
+
+function renderInMenu(dishObj) {
+    const menuItem = document.createElement('span');
+    menuItem.textContent = dishObj.name;
+    menuList.append(menuItem)
+}
+
